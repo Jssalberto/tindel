8bec77be85927cba6e8af8466c9f0ece47b2883c
alberto <jesusalbertojimenez500@gmail.com>
2025-06-04 23:20:49 -0600

    tindel 1.1 v route_certificate_firebase

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
 
 def iniciar_firebase():
-    cred = credentials.Certificate("credenciales-firebase.json")
+    ruta = os.path.expanduser("~/Escritorio/credenciales-firebase.json")
+    cred = credentials.Certificate(ruta)
+    # cred = credentials.Certificate("credenciales-firebase.json")
     firebase_admin.initialize_app(cred)
     return firestore.client()
