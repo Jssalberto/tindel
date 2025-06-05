72b6aebaf9e5b439a885d199d141cb0f51eca0c5
alberto <jesusalbertojimenez500@gmail.com>
2025-06-04 23:43:34 -0600

    modify certificate-firebase

--- firebase_config.py
+++ firebase_config.py
@@ -5,6 +5,5 @@ import os
 def iniciar_firebase():
     ruta = os.path.expanduser("~/Escritorio/credenciales-firebase.json")
     cred = credentials.Certificate(ruta)
-    # cred = credentials.Certificate("credenciales-firebase.json")
     firebase_admin.initialize_app(cred)
     return firestore.client()
